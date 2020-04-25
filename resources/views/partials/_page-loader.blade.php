74cfa16ed49b0b64dd75d364455f96f5f7984802
amir71 <sa.shojaei92@gmail.com>
2020-04-26 02:39:29 +0430

    Add tennis png

--- resources/views/partials/_page-loader.blade.php
+++ resources/views/partials/_page-loader.blade.php
@@ -1,7 +1,7 @@
 
 <!-- begin::Page loader -->
 <div class="kt-page-loader kt-page-loader--logo">
-	<img id="tennis-loading"  alt="Logo" src="{{asset('images/tennis1.png')}}" />
+	<img id="tennis-loading"  alt="Logo" src="{{asset('images/tennis-64x64.png')}}" />
 </div>
 
 <!-- end::Page Loader -->
